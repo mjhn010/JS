76d7ca77d1d7d8abfe7ae3c61184b8d1e8b74f97
mjhn010 <mjhn010@naver.com>
2023-06-15 17:05:54 +0900

    블록쌓기, 블록랜덤하게생성

--- js/tetris.js
+++ js/tetris.js
@@ -1,4 +1,8 @@
+import blocks from "./block.js ";
+
+// window.addEventListener("load",()=>{
 const playground = document.querySelector(".playground > ul")
+
 // setting
 let gameRow = 20;
 let gameCol = 10;
@@ -8,18 +12,18 @@ let dropSpeed = 500;
 let downInterval;
 let tempMovingItem;
 
-const blocks ={
-    // 우리가 알고있는 ㅗ 모양(욕아님)
-    // ㅗ 이모양은 총 위,아래,좌,우 방향키를 누를때마다 모양이 다 다르므로 tree라는 배열에 4공간 배열을 만들어준다.
+// const blocks ={
+//     // 우리가 알고있는 ㅗ 모양(욕아님)
+//     // ㅗ 이모양은 총 위,아래,좌,우 방향키를 누를때마다 모양이 다 다르므로 tree라는 배열에 4공간 배열을 만들어준다.
 
-    tree:[
-        [[2,1],[0,1],[1,0],[1,1]],
-        [],
-        [],
-        [],
-    ]
+//     tree:[
+//         [[2,1],[0,1],[1,0],[1,1]],
+//         [[2,1],[1,2],[1,0],[1,1]],
+//         [[1,2],[0,1],[2,1],[1,1]],
+//         [[1,2],[0,1],[1,0],[1,1]],
+//     ]
 
-}
+// }
 
 
 const movingItem = {
@@ -37,18 +41,24 @@ init();
 
 // 초기화
 function init(){
-    // 오디오 추가 
-    let audio = document.getElementById("audio");
-    // 자동재생
-    audio.autoplay = true;
-    // 무한재생
-    audio.loop = true;
     tempMovingItem = {...movingItem};
     // board 그리기 row = 20, col = 10;
 for(let i = 0; i<gameRow; i++){
     prependNewLine();
 }
 rendering()
+}
+
+
+// audio
+
+function musicAudio(){
+// 오디오 추가 
+let audio = document.getElementById("audio");
+// 자동재생
+audio.autoplay = true;
+// 무한재생
+audio.loop = true;
 }
 
 // 라인 그려주기 
@@ -66,17 +76,15 @@ function prependNewLine(){
 }
 
 
-function rendering(){
+function rendering(moveType){
     // Destructuring
     let {type,direction,top,left} = tempMovingItem;
-    // console.log(type,direction)
-    // console.log(blocks[type][direction])
     let moving = document.querySelectorAll(".moving")
     for (let move of moving) {
         move.classList.remove(type,"moving")
         console.log(move)
     }
-    blocks[type][direction].forEach(element => {
+    blocks[type][direction].some(element => {
         let x = element[0] + left
         let y = element[1] + top
         // console.log(playground.childNodes)
@@ -90,23 +98,59 @@ function rendering(){
         // console.log(target)
         setTimeout(()=>{
             rendering();
-        })
+            if(moveType === "top"){
+                seize();
+            }
+        },0)
+        return true;
         } 
-            
     });
+    movingItem.left = left;
+    movingItem.top = top;
+    movingItem.direction = direction;   
 }
 
 function checkEmpty(target){
-    if(!target)
+    if(!target || target.classList.contains("seized"))
         return false;
     else return true;
 }
 
+function seize(){
+    let moving = document.querySelectorAll(".moving");
+    for (let move of moving) {
+        move.classList.remove("moving");
+        move.classList.add("seized");
+    }
+    generateNew();
+}
+// 블록이 닿으면 새로운 블록 생성하기.
+function generateNew(){
+    // 랜덤 블록 생성
+    // 오브젝트 entrie을 통해서 오브젝트를 숫자로 변경
+    // 왜냐 ? length 길이만큼 랜덤하게 돌리면서 블록을 불러오고싶기떄문.
+    let blockArray = Object.entries(blocks);
+    console.log(blockArray)
+    let random = Math.floor(Math.random()*blockArray.length)
+    movingItem.type= blockArray[random][0]
+
+    movingItem.top = 0;
+    movingItem.left = 3;
+    movingItem.direction = 0;
+    tempMovingItem = {...movingItem}
+    rendering();
+}
 
 function move(moveType,amount){
     tempMovingItem[moveType] += amount;
-    rendering();
+    rendering(moveType);
 
+}
+function chageDirection(){
+    // 블록 돌리기 
+   tempMovingItem.direction === 3 ? tempMovingItem.direction = 0 : tempMovingItem.direction += 1;
+   console.log(tempMovingItem.direction)
+   rendering();
 }
 
 document.addEventListener("keydown",(e) =>{
@@ -117,11 +161,14 @@ document.addEventListener("keydown",(e) =>{
         // 오른쪽 키코드 = 39
         case 39:move("left",1);
             break;
-        // 하지만 테트리스에서는 위로 올라갈수없다 그래서 아래는 위 이동키는 구현xx !
-        // case 38 :move("top",-1);
-        // break;
+        case 38 :chageDirection(); 
+        break;
         // 아래
         case 40: move("top", 1);
             break;
+        default: break;
     }
 })
+
+
+// })
